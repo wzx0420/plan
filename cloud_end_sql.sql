5202a4e001c5fa3e337878b7cf2339bb4d1ee7f9
wing <wing@turingcat.com>
2017-11-23 11:44:33 +0800

    sql脚本

---
+++ cloud_end_sql.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Ultimate v12.2.6 (64 bit)
+MySQL - 5.7.20 : Database - cloud_end
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`cloud_end` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `cloud_end`;
+
+/*Table structure for table `tb_user` */
+
+DROP TABLE IF EXISTS `tb_user`;
+
+CREATE TABLE `tb_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id（自增序列）',
+  `user_name` varchar(50) DEFAULT NULL COMMENT '用户昵称',
+  `login_name` varchar(50) DEFAULT NULL COMMENT '登录账号',
+  `login_pwd` varchar(100) DEFAULT NULL COMMENT '登录密码',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `login_out_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最后登录时间',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tb_user` */
+
+insert  into `tb_user`(`user_id`,`user_name`,`login_name`,`login_pwd`,`create_time`,`update_time`,`login_out_time`) values 
+(1,'管理员admin','admin','admin','2017-11-21 16:17:28','2017-11-21 16:18:25','2017-11-21 16:17:28');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
